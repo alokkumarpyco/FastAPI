92363438d6c9bc0bd802e192785f54f0bb5d0869
alokkumarpyco <kumar.alok4241@gmail.com>
2023-07-18 02:05:00 +0900

    added functions

---
+++ TestPackage/generate_number.py
@@ -0,0 +1,6 @@
+import random
+
+
+def generate_num():
+    num = random.randint(10, 20)
+    return num
--- books.py
+++ books.py
@@ -1,13 +1,14 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
+from TestPackage import generate_number
 
 app = FastAPI()
 
 BOOKS = [
     {"title": "Title One", "author": "Author One", "category": "science"},
-    {"title": "Title Two", "author": "Author Two", "category": "science"},
+    {"title": "Title Two", "author": "Author Two", "category": "math"},
     {"title": "Title Three", "author": "Author Three", "category": "history"},
     {"title": "Title Four", "author": "Author Four", "category": "science"},
-    {"title": "Title Five", "author": "Author Five", "category": "science"},
+    {"title": "Title Five", "author": "Author Five", "category": "math"},
     {"title": "Title Six", "author": "Author Two", "category": "math"},
 ]
 
@@ -33,3 +34,83 @@ async def read_category_by_query(category: str):
         if book.get("category").casefold() == category.casefold():
             book_to_return.append(book)
     return book_to_return
+
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str):
+    book_to_return = []
+    for book in BOOKS:
+        if (
+            book.get("author").casefold() == book_author.casefold()
+            and book.get("category").casefold() == category.casefold()
+        ):
+            book_to_return.append(book)
+    return book_to_return
+
+
+@app.post("/books/create/books")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
+    return new_book
+
+
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get("title").casefold() == updated_book.get("title").casefold():
+            BOOKS[i] = updated_book
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@app.get("/books/numbers/")
+async def get_a_random_number():
+    num = get_a_random_number()
+    return num
